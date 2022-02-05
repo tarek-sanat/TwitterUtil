bad5773ac5d3d576096286003bee7c2a7b4ef822
nothing nothing <tareksanat@oakland.edu>
2022-02-05 17:10:28 -0500

    Deleted Unused Route
    Deletion of Unused Route

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 var getSongRouter = require('./routes/getSong');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/getSong', getSongRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
